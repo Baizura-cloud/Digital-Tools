74d54836b5c801617980b54896f1d31539394e1c
baizura <baizura1996@gmail.com>
2023-05-09 18:01:16 +0800

    currency conversion logic

--- src/features/conversion/conversion.js
+++ src/features/conversion/conversion.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import CardContent from '@mui/material/CardContent';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -11,16 +11,50 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import axios from 'axios';
+import Button from '@mui/material/Button';
 
 const theme = createTheme();
 
 
 export default function Conversion() {
-  const [age, setAge] = useState('');
+  const [from, setFrom] = useState('usd');
+  const [to, setTo] = useState('myr');
+  const [rate, setRate] = useState('')
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  // useEffect(() => {
+  //   axios.get('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json')
+  //     .then((response) => {
+  //       setRate(response.data)
+  //     })
+  // }, [])
+
+  const changeFrom = (event) => {
+    setFrom(event.target.value);
   };
+  const changeTo = (event) => {
+    setTo(event.target.value);
+  }
+
+  function _callRate() {
+    try {
+      axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${to}.json`)
+        .then((response) => {
+          setRate(response.data)
+          calculateConvert()
+        })
+    } catch (error) {
+        alert("invalid data")
+    }
+  }
+
+  function calculateConvert(){
+    console.log(rate[to])
+    const currentRate = rate[to];
+    const amount = document.getElementById('amount').value;
+    var convert = currentRate*amount;
+    document.getElementById('convAnswer').value = convert
+  }
 
   return (
     <div >
@@ -33,52 +67,55 @@ export default function Conversion() {
                 <Stack direction="column" spacing={4}>
                   <Grid item xs={12}>
                     <Stack direction="row" spacing={4}>
-                    <TextField
-                      id="answer"
-                      name="answer"
-                      type="number"
-                      label="Amount"
-                    />
-                    <InputLabel id="demo-simple-select-label" >From</InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      value={age}
-                      label="Age"
-                      onChange={handleChange}
-                      sx={{ minWidth: 150 }}
-                    >
-                      <MenuItem value={10}>TenTenTenTenTenTenTen</MenuItem>
-                      <MenuItem value={20}>TenTenTenTenTenTenTenTen</MenuItem>
-                      <MenuItem value={30}>TenTenTenTenTenTenTenTen</MenuItem>
-                    </Select>
-                    <InputLabel  id="demo-simple-select-label" >To</InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      value={age}
-                      label="Age"
-                      onChange={handleChange}
-                      sx={{ minWidth: 150 }}
-                    >
-                      <MenuItem value={10}>TenTenTenTenTenTenTen</MenuItem>
-                      <MenuItem value={20}>TenTenTenTenTenTenTenTen</MenuItem>
-                      <MenuItem value={30}>TenTenTenTenTenTenTenTen</MenuItem>
-                    </Select>
+                      <TextField
+                        id="amount"
+                        name="amount"
+                        type="number"
+                        label="Amount"
+                      />
+                      <InputLabel id="demo-simple-select-label" >From</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={from}
+                        label="From"
+                        onChange={changeFrom}
+                        sx={{ minWidth: 150 }}
+                      >
+
+                        <MenuItem value={"usd"}>usd</MenuItem>
+                        <MenuItem value={"myr"}>myr</MenuItem>
+                        <MenuItem value={"sgd"}>sgd</MenuItem>
+                      </Select>
+                      <InputLabel id="demo-simple-select-label" >To</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={to}
+                        label="To"
+                        onChange={changeTo}
+                        sx={{ minWidth: 150 }}
+                      >
+                        <MenuItem value={"usd"}>usd</MenuItem>
+                        <MenuItem value={"myr"}>myr</MenuItem>
+                        <MenuItem value={"sgd"}>sgd</MenuItem>
+                      </Select>
                     </Stack>
-                    
+
                   </Grid>
-                  <Stack direction="row" spacing={4}>
-                    <TextField
-                      id="answer"
-                      name="answer"
-                      type="number"
-                      inputProps={
-                        { readOnly: true, }
-                      }
-                      label="Converted Amount"
-                    />
+                  <Grid item xs={12}>
+                    <Stack direction="row" spacing={4}>
+                      <Button onClick={() => _callRate()} variant="contained" color="success">Convert</Button>
+                      <TextField
+                        id="convAnswer"
+                        name="convAnswer"
+                        type="number"
+                        inputProps={
+                          { readOnly: true, }
+                        }
+                      />
                     </Stack>
+                  </Grid>
                 </Stack>
               </CardContent>
             </Card>
